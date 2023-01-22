ada25069e8e1952f91b28c8107e20aa3b69accb3
KDS <kddddds@gmail.com>
2023-01-22 05:49:25 +0000

    npm install svelte-spa-router 설치 svelte 라우터 작성

---
+++ fronted/src/routes/Home.svelte
@@ -0,0 +1,19 @@
+<script>
+    let question_list = []
+
+    function get_question_list() {
+        fetch("https://ya2301.ebesesk.synology.me/api/question/list").then((response) => {
+            response.json().then((json) => {
+                question_list = json
+            })
+        })
+    }
+
+    get_question_list()
+</script>
+
+<ul>
+    {#each question_list as question}
+        <li>{question.subject}</li>
+    {/each}
+</ul>
